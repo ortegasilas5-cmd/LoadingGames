9dea5a8099f39268eb45a987a897a95cb51609af
Silas <ortegasilas5@gmail.com>
2026-09-09 21:18:29 -0300

    feat: adicionando gtav e reddead 2

---
+++ LoadingGames/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoadingGames.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewData["Id"] = id;
+
+            return View();
+        }
+    }
+}
