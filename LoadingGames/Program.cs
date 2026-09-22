d1f6a78b3a738413a66b57829b68e7a48834a208
Yasmim Santos <yasmimgrilo@hotmail.com>
2026-09-21 22:18:38 -0300

    Feat: Login/Senha (Front)

--- LoadingGames/Program.cs
+++ LoadingGames/Program.cs
@@ -6,45 +6,59 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Adiciona o banco de dados MySQL
 builder.Services.AddDbContext<LoadingGamesContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(
-            builder.Configuration.GetConnectionString("DefaultConnection")
-        )
-    )
+	options.UseMySql(
+		builder.Configuration.GetConnectionString("DefaultConnection"),
+		ServerVersion.AutoDetect(
+			builder.Configuration.GetConnectionString("DefaultConnection")
+		)
+	)
 );
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configuração da conexão
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(
-        connectionString,
-        ServerVersion.AutoDetect(connectionString)
-    )
+	options.UseMySql(
+		connectionString,
+		ServerVersion.AutoDetect(connectionString)
+	)
 );
+
+// ==========================================
+// CONFIGURAÇÃO DA SESSION
+// ==========================================
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
 app.UseRouting();
+
+// ==========================================
+// ATIVA A SESSION
+// ==========================================
+app.UseSession();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}")
+	.WithStaticAssets();
 
 app.Run();
